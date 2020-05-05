8250ea0d7a38a44fc697afb3d58c400e022b1a3e
Rashmi Gupta <rashmi224141@gmail.com>
2020-05-05 23:17:39 +0530

    Added test for insert_at

--- test/test.c
+++ test/test.c
@@ -22,20 +22,6 @@ void print_result(int result)
   NEW_LINE;
 }
 
-void test_add_to_start()
-{
-  printf("# add_to_start");
-  NEW_LINE;
-  List_ptr list = create_list();
-  int previous_length = list->count;
-  printf("should add the number at the start of the list : ");
-  Status status = add_to_start(list, 5);
-  int result = status == Success && list->count == previous_length + 1 && is_number_present_at(list, 0, 5);
-  print_result(result);
-  destroy_list(list);
-  NEW_LINE;
-}
-
 void test_add_to_end()
 {
   NEW_LINE;
@@ -51,9 +37,58 @@ void test_add_to_end()
   NEW_LINE;
 }
 
+void test_add_to_start()
+{
+  printf("# add_to_start");
+  NEW_LINE;
+  List_ptr list = create_list();
+  int previous_length = list->count;
+  printf("should add the number at the start of the list : ");
+  Status status = add_to_start(list, 5);
+  int result = status == Success && list->count == previous_length + 1 && is_number_present_at(list, 0, 5);
+  print_result(result);
+  destroy_list(list);
+  NEW_LINE;
+}
+
+void test_insert_at()
+{
+  printf("# insert_at");
+  NEW_LINE;
+  List_ptr list = create_list();
+  add_to_end(list, 1);
+  add_to_end(list, 2);
+
+  int previous_length = list->count;
+  printf("should insert at 0th position in the list : ");
+  Status status = insert_at(list, 3, 0);
+  int result = status == Success && list->count == previous_length + 1 && is_number_present_at(list, 0, 3);
+  print_result(result);
+
+  previous_length = list->count;
+  printf("should insert at last position in the list : ");
+  status = insert_at(list, 4, 3);
+  result = status == Success && list->count == previous_length + 1 && is_number_present_at(list, 3, 4);
+  print_result(result);
+
+  previous_length = list->count;
+  printf("should insert at given position in the list : ");
+  status = insert_at(list, 5, 2);
+  result = status == Success && list->count == previous_length + 1 && is_number_present_at(list, 2, 5);
+  print_result(result);
+
+  previous_length = list->count;
+  printf("should not insert if the given position is invalid : ");
+  status = insert_at(list, 6, 10);
+  result = status == Failure && list->count == previous_length && !is_number_present_at(list, 10, 6);
+  print_result(result);
+
+  destroy_list(list);
+  NEW_LINE;
+}
+
 void test_add_unique()
 {
-  NEW_LINE;
   printf("# add_unique");
   NEW_LINE;
   List_ptr list = create_list();
@@ -128,6 +163,7 @@ void run_tests()
   test_remove_from_start();
   test_remove_from_end();
   test_add_unique();
+  test_insert_at();
 }
 
 int main(void)
